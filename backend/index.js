b68f67636e896e3f180a49eb3cc8add87e01605a
Joy khulbe <khulbejoy.jk@gmail.com>
2024-03-10 12:35:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
-import express from "express";
+import express, { response } from "express";
 import { PORT } from "./config.js";
 
 const app = express();
+
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Working Sucessfully");
+});
 
 app.listen(PORT, () => {
   console.log(`App is Running on Port ${PORT}`);
