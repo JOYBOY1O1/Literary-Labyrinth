46b4b5a826f349e7ecc74457c1baba3a57fd5716
Joy khulbe <khulbejoy.jk@gmail.com>
2024-03-10 12:45:23 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const mongoDBURL =
+  "mongodb+srv://root:root@literary-labyrinth.9rdeurl.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Literary-Labyrinth";
