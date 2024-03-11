54738dba61ce9f02c3fa951c54fbe9d25cdf328e
Joy khulbe <khulbejoy.jk@gmail.com>
2024-03-11 12:38:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express, { response } from "express";
+import express, { request, response } from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose, { mongo } from "mongoose";
 import { Book } from "./models/bookModel.js";
@@ -8,6 +8,30 @@ const app = express();
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Working Sucessfully");
+});
+
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title,author,publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
